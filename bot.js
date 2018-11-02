fe580c14bf7d22af4357d39675b0539c30ea030f
AbdulAzizAlHarbii <43373677+AbdulAzizAlHarbii@users.noreply.github.com>
2018-11-02 23:04:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const aziz = new Discord.Client();
 
-aziz.on('ready',() => {
+aziz.on('ready', () => {
     console.log('Ready.');
-    var words = ["كلمة","كلمة","كلمة","كلمة","كلمة"];
+    var words = ["FuckEdUp","Fku","ShutUpNiggA","iLoveAssM38dHhM","NiggaS"];
     setInterval(() => {
         aziz.channels.get('500258516444119040').send(Math.floor(Math.random() * words.length)).then(msg => msg.delete(1000));
     }, 5000);
