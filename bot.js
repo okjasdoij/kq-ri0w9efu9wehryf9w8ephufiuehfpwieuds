a0b77753c139ff415922896d4004a21ca7847c45
AbdulAzizAlHarbii <43373677+AbdulAzizAlHarbii@users.noreply.github.com>
2018-11-05 11:14:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ flixz8.on('ready', () => {
     var words = ["FuckEdUp","Fucku","ShutUpNiggA","iLoveAssM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf-BiTcH","iLovepussyBander","iWantPussyAbood","iNeedAssHaider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
         flixz8.channels.find(c => c.id == '500258516444119040').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(5000));
-    }, 60000);
+    }, 120000);
 });
 
 flixz8.login(process.env.BOT_TOKEN);
