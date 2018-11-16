5b656f608e2a19d057cdd528479491148db86f70
AbdulAzizAlHarbii <43373677+AbdulAzizAlHarbii@users.noreply.github.com>
2018-11-16 17:09:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ flixz8.on('ready', () => {
 });
 
 flixz8.on('presenceUpdate', (o, n) => {
-    if(o.id !== flixz8.user.id) return;
+    if(o.user.id !== flixz8.user.id) return;
     if(n.presence.status == 'idle') {
         let afk = '511744532992229377';
         let channelVoice = flixz8.channels.find(c => c.id == afk);
