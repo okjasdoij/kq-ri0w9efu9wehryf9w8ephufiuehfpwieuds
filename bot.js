43f3584cd185f7fd52421df765f4f6884aef4876
AbdulAzizAlHarbii <43373677+AbdulAzizAlHarbii@users.noreply.github.com>
2018-11-19 01:54:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
 const Discord = require('discord.js');
-const flixz8 = new Discord.Client();
+const cilent = new Discord.Client();
 
-flixz8.on('ready', () => {
+cilent.on('ready', () => {
     console.log('Ready.');
     var words = ["87987","ijdojsdosij","ShutUpNiggA","iLovey","NiggauS","NoMerrCy-Nigga","Assaf-uH","iLovepunur","iWantuAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
-        flixz8.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1000));
+        cilent.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1000));
     }, 2500);
 });
 
-flixz8.on('ready', () => {
-  console.log(`Logged in as ${flixz8.user.tag}!`);
+cilent.on('ready', () => {
+  console.log(`Logged in as ${cilent.user.tag}!`);
 });
 
-flixz8.on('message', msg => {
+cilent.on('message', msg => {
   if (msg.content === ' السلام عليكم ') {
     msg.reply(' وعليكم السلام ورحمة الله وبركاته ');
   }
