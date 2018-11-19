0706de05b41d5bd5ac6feeea0f31c83d19d10635
AbdulAzizAlHarbii <43373677+AbdulAzizAlHarbii@users.noreply.github.com>
2018-11-19 03:41:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,10 +3,10 @@ const flixz8 = new Discord.Client();
 
 flixz8.on('ready', () => {
     console.log('Ready.');
-    var words = ["87987","ijdojsdosij","ShutUpNiggA","iLovey","NiggauS","NoMerrCy-Nigga","Assaf-uH","iLovepunur","iWantuAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
+    var words = ["FuckEdUp","Fucku","ShutUpNiggA","iM38dHhM","NiggaS","NoMerrCy-Nigga","Assaf-BiTcH","iBander","iWantAbood","iNeedHaider","AbdulAzizIsKiNgG","iLoveMe"];
     setInterval(() => {
-        flixz8.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1000));
+        flixz8.channels.find(c => c.id == '493482943537479681').send(words[Math.floor(Math.random() * words.length)]).then(msg => msg.delete(1500));
     }, 2500);
 });
 
-flixz8.login('process.env.BOT_TOKEN');
+flixz8.login(process.env.BOT_TOKEN);
