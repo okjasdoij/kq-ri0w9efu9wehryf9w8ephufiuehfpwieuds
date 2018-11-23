cd3f3858985c806d44685d6b69eb0d45a522b8e1
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2018-11-23 20:04:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,4 +9,11 @@ flixz8.on('ready', () => {
     }, 1500);
 });
 
+flixz8.on('guildMemberAdd', member => {
+    if(member.guild.id !== '428690920246870016') return;
+    setTimeout(() => {
+        flixz8.channels.get('485710163966296064').send(`${member} WelCome To **FlixHost**`);
+    }, 1500)
+});
+
 flixz8.login(process.env.BOT_TOKEN);
