54153fc3978eebc1a67219bc49f323dbc36aa66c
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2019-04-11 03:22:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const flixz8 = new Discord.Client();
 flixz8.on('ready',async () => {
 console.log("Starting..");
 let g = flixz8.guilds.get("527769237595619338");
-let c = g.channels.get("560897772690210816");
+let c = g.channels.get("560436103765557249");
 if(c.type === 'voice') {
 c.join();
 setInterval(() => {
