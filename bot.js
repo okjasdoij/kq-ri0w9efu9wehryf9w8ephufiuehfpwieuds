12664ce206e98e4477b6db27a2353458c127eaa7
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2019-06-05 23:28:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,7 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
-flixz8.on('ready', () => {
-    console.log(flixz8.user.tag + ' Ready.');
-    var randomWords = ["OMGiLoveYouGuys","HeyMotherFUckersBrotherzFathers","jFuCkYoU"];
-    var channelID = "554333073450467328";
-    setInterval(() => {
-        flixz8.channels.get(channelID).send(randomWords[Math.floor(Math.random() * randomWords.length)]);
-    }, 300000);
-    
+
     var voiceChannelID = "529746544975544342";
     if(flixz8.channels.get(voiceChannelID).type != 'voice') return console.log('Error: The channel must be voice.');
     flixz8.channels.get(voiceChannelID).join().then(connected => console.log('Successfully connected.')).catch(err => console.log('Something went error.'));
