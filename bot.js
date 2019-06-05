975fc3bea760dbc766d78e9f207c9cc90e483ccf
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2019-06-05 21:46:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,20 @@
 const Discord = require('discord.js');
 const flixz8 = new Discord.Client();
 
-
-flixz8.on('ready',async () => {
-console.log("Starting..");
-let g = flixz8.guilds.get("211543198651121664");
-let c = g.channels.get("529746544975544342");
-if(c.type === 'voice') {
-c.join();
-setInterval(() => {
-if(!g.me.voiceChannel) c.join();
-}, 1);
-} else {
-console.log("Failed To Join:\n The Channel Type isn't \"text\"");
-}
+flixz8.on('ready', () => {
+    console.log(flixz8.user.tag + ' Ready.');
+    var randomWords = ["1","2","3"];
+    var channelID = "554333073450467328";
+    setInterval(() => {
+        flixz8.channels.get(channelID).send(randomWords[Math.floor(Math.random() * randomWords.length)]);
+    }, 300000);
+    
+    var voiceChannelID = "Voice ID";
+    if(flixz8.channels.get(voiceChannelID).type != 'voice') return console.log('Error: The channel must be voice.');
+    flixz8.channels.get(voiceChannelID).join().then(connected => console.log('Successfully connected.')).catch(err => console.log('Something went error.'));
+    setInterval(() => {
+        if(!flixz8.guilds.get('211543198651121664').me.voiceChannel) flixz8.channels.get(voiceChannelID).join();
+    }, 5000);
 });
 
-
-flixz8.login(process.env.BOT_TOKEN);
+flixz8.login('TOKEN');
