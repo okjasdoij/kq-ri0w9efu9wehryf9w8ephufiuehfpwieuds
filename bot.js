dec965e1e009deea93a77b6dcd6fd45ea0c81283
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2019-12-28 11:51:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js');
-const flixz8 = new Discord.Client();
+const flixz9 = new Discord.Client();
 
-flixz8.on('ready', () => {
+flixz9.on('ready', () => {
     var voiceChannelID = "660177935151661106";
-    if(flixz8.channels.get(voiceChannelID).type != 'voice') return console.log('Error: The channel must be voice.');
-    flixz8.channels.get(voiceChannelID).join().then(connected => console.log('Successfully connected.')).catch(err => console.log('Something went error.'));
+    if(flixz9.channels.get(voiceChannelID).type != 'voice') return console.log('Error: The channel must be voice.');
+    flixz9.channels.get(voiceChannelID).join().then(connected => console.log('Successfully connected.')).catch(err => console.log('Something went error.'));
     setInterval(() => {
-        if(!flixz8.guilds.get('527769237595619338').me.voiceChannel) flixz8.channels.get(voiceChannelID).join();
+        if(!flixz9.guilds.get('527769237595619338').me.voiceChannel) flixz8.channels.get(voiceChannelID).join();
     }, 10000);
 });
 
-flixz8.login(process.env.BOT_TOKEN);
+flixz9.login(process.env.BOT_TOKEN);
