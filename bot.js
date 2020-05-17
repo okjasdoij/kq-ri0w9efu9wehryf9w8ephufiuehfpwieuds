ff894dce79aeb7e6940bc5084353f25b49f54b22
okjasdoij <45161348+okjasdoij@users.noreply.github.com>
2020-05-17 20:08:36 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,12 @@
-const Discord = require('discord.js');
-const flixz9 = new Discord.Client();
+const { Client } = require("discord.js");
+const flixz9 = new Client();
 
-flixz9.on('ready', () => {
-    var voiceChannelID = "527769237595619338";
-    if(flixz9.channels.get(voiceChannelID).type != 'voice') return console.log('Error: The channel must be voice.');
-    flixz9.channels.get(voiceChannelID).join().then(connected => console.log('Successfully connected.')).catch(err => console.log('Something went error.'));
-    setInterval(() => {
-        if(!flixz9.guilds.get('660177935151661106').me.voiceChannel) flixz8.channels.get(voiceChannelID).join();
-    }, 1000);
+flixz9.on("message", message => {
+    if(message.author.id !== "294882584201003009") return;
+    if(!message.content.includes("GIVEAWAY")) return;
+    setTimeout(() => {
+        message.react(":tada:");
+    }, 20000);
 });
 
 flixz9.login(process.env.BOT_TOKEN);
